0341fdcab7eb264dbbfdf5ec2c3163a7377d8d94
Gustavo Rodrigues <gustavohalves@gmail.com>
2024-05-06 10:41:55 -0300

    Criado o servidorcom api

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.get('/',(req,res)=>{
+	res.status(200).send({message:'Olá Mundo estou funcionando'});
+})
+app.listen(3001,()=>{
+	console.log('Api rodando na porta 3001')
+})
