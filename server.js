953036f77bd56888cf3447816c7748bde383c3c6
Luiz Gustavo Gomes Rodrigues <gustavohalves@gmail.com>
2024-05-06 10:56:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 app.get('/',(req,res)=>{
 	res.status(200).send({message:'Olá Mundo estou funcionando'});
 })
+
+app.get('/',(req,res)=>{
+	res.status(200).send({message:'Olá , eu sou um rota alternativa'});
+})
 app.listen(3001,()=>{
 	console.log('Api rodando na porta 3001')
 })
